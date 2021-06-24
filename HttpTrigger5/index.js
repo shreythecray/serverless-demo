f00c8d4f32863c32cf0c40d8e94300ec80e504a0
shreythecray <shrgupta@ucdavis.edu>
2021-06-24 00:41:23 -0700

    DEPLOY THE DAMN CODE SHREYA

--- HttpTrigger5/index.js
+++ HttpTrigger5/index.js
@@ -9,10 +9,9 @@ module.exports = async function (context, req) {
     
     let data = await resp.arrayBuffer();
 
-    let base64data = Buffer.from(data).toString('base64')
+    let base64data = Buffer.from(data).toString('base64');
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: { base64data }
     };
 }
