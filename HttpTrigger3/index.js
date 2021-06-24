90f09e0db7186ac1b21d98cba7be5c54d552e5b6
shreythecray <shrgupta@ucdavis.edu>
2021-06-23 23:42:31 -0700

    updated hackervoice function to check for correct password

--- HttpTrigger3/index.js
+++ HttpTrigger3/index.js
@@ -3,8 +3,19 @@ module.exports = async function (context, req) {
 
     const password = req.query.password || req.body.password
 
+    let your_response;
+
+    if(password == "letmein") {
+        your_response = "Access granted."
+    } else {
+        your_response = "Access denied."
+    }
+
+    // let your_response = (password == "letmein") ?
+    //     "Access granted." : "Access denied."
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: your_response
     };
 }
