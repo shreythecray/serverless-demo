b507925bb096d20a380e0c73383b37b5f1e06308
shreythecray <shrgupta@ucdavis.edu>
2021-06-24 00:27:04 -0700

    created the twocatz function which returns an image of a cat in base64

---
+++ HttpTrigger5/index.js
@@ -0,0 +1,20 @@
+const fetch = require('node-fetch')
+
+module.exports = async function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    let resp = await fetch("https://cataas.com/cat/cute/says/Serverless", {
+        method: 'GET'
+    });
+    
+    let data = await resp.arrayBuffer();
+
+    let base64data = Buffer.from(data).toString('base64')
+
+    context.res = {
+        // status: 200, /* Defaults to 200 */
+        body: base64data
+    };
+}
+
+//https://cataas.com/cat/cute/says/Serverless
