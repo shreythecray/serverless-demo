6708d410e5aadc288a5bd9a768a196ded258059a
shreythecray <shrgupta@ucdavis.edu>
2021-07-02 03:07:52 -0700

    fixed format in which the response was being sent

--- emotionalgifs/index.js
+++ emotionalgifs/index.js
@@ -16,7 +16,7 @@ module.exports = async function (context, req) {
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: { main_emotion }
+        body: main_emotion
     };
 }
 
