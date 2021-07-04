48440e93daee611acfef86969ccf8a11f86ce501
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-07-03 23:09:05 -0400

    Update test.3.5.js

--- .bit/tests/test.3.5.js
+++ .bit/tests/test.3.5.js
@@ -44,7 +44,7 @@ if (uri1[0] == null || uri2[1] == null) {
                 console.log("Yay! 🎉 We got our picture!")
             } else {
                 console.log("Hmmm... Maybe take another look at that download function.")
-                console.log("We got: "message.downloadUri)
+                console.log("We got: " + message.downloadUri)
                 console.log("We should have got: " + `${blob_url}/${containerName}/bunnimagetestrun1001.jpeg`)
                 process.exit(1)
             }
