aa3090dedc20f237c1b4e0749972b7527eb1d576
shreythecray <shrgupta@ucdavis.edu>
2021-07-03 18:41:24 -0700

    updated bunnimage

--- bunnimage-upload/index.js
+++ bunnimage-upload/index.js
@@ -1,13 +1,42 @@
+const multipart = require('parse-multipart');
+const fetch = require('node-fetch');
+
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const { BlobServiceClient } = require("@azure/storage-blob");
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    var body = req.body;
+    var parsedBody = multipart.Parse(body, boundary);
+
+    var filetype = parsedBody[0].type;
+    if (filetype == "image/png") {
+        ext = "png";
+    } else if (filetype == "image/jpeg") {
+        ext = "jpg";
+    } else {
+        username = "invalidimage"
+        ext = "";
+    }
+
+    var responseMessage = await uploadFile(parsedBody, ext);
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
+}
+
+async function uploadFile(parsedBody, ext) {
+    const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
+    const containerName = "images";
+    const containerClient = blobServiceClient.getContainerClient(containerName);    // Get a reference to a container
+
+    const blobName = 'test.' + ext;    // Create the container
+    const blockBlobClient = containerClient.getBlockBlobClient(blobName); // Get a block blob client
+
+    const uploadBlobResponse = await blockBlobClient.upload(parsedBody[0].data, parsedBody[0].data.length);
+
+    return ("Your blob is saved");
 }
