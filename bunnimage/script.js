9fc4fc50e7a4a8180c90a662579fecd6f4048aa3
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-07-14 14:20:57 -0400

    Update script.js

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,6 +1,11 @@
-function getImage() {
-    if (document.getElementById("name").value != '') {
-        $('#output').text(document.getElementById("name").value + "❤️")
-        console.log("high")
+function getImage(event) {
+    event.preventDefault()
+    var myform = document.getElementById("myform")
+    var payload = new FormData(myform);
+    console.log(payload)
+    if (document.getElementById("username").value != '') {
+        $('#output').text("Thanks!")
+    } else {
+        alert("No name error.")
     }
   }
