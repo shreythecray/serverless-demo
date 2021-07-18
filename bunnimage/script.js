87bdc79d9cadd9812699b00a91dc62afbb2bfbd8
Emily Chen <69332964+emsesc@users.noreply.github.com>
2021-07-17 21:08:59 -0400

    Update script.js

--- bunnimage/script.js
+++ bunnimage/script.js
@@ -1,80 +1,45 @@
-function getImage(event) {
-    event.preventDefault()
-    var myform = document.getElementById("myform")
-    //get image and filename uploaded by user via the form
-    let nameInput = document.getElementById("username");
-    let fileInput = document.getElementById("image");
-    let file = fileInput.files[0]; //get image data
+async function getImage(event) {
+  event.preventDefault()
+  var myform = document.getElementById("myform")
+  var payload = new FormData(myform);
+  console.log(payload)
+  var username = document.getElementById("username").value;
+  if (username != '') {
+      $('#output').text("Thanks!")
 
-    var payload = new FormData(myform);
-    console.log(payload)
-    payload.append("file", file);
-    $('#output').text("Thanks!")
-    
-    if (document.getElementById("username").value != '') {
+      console.log("Posting your image...");
+      const resp = await fetch("YOUR_URL", {
+          method: 'POST',
+          headers: {
+              'codename' : username
+          },
+          body: payload
+      });
 
-        try {
-            let url = "https://priscool.azurewebsites.net/api/w3s3?code=WXnJgVXmLh1VRmcNyZhyr9yy5GSu94iTuEsvSI0chEoHmWazwBK/dg=="
-            console.log("Image was uploaded, making POST request to Azure function")
-            //create request to Azure function
-            const resp = fetch(url, {
-                method: 'POST',
-                headers: {
-                    'codename': nameInput.value
-                },
-                body: payload
-            })
-            console.log("resp:" + resp)
-
-            console.log("POST request was made successfully")
-            $('#output').text("Your image has been stored successfully!")
-        } catch(err) {
-            $('#output').text(err)
-        }
-
-    } else {
-        alert("No name error.")
-    }
-
-}
-
-async function downloadImage() {
-  let username = document.getElementById("downloadusername").value
-  console.log("username:" + username)
-  
-  if(username != '') {
-
-    try {
-      let url = "https://priscool.azurewebsites.net/api/w3s5?code=RMTTWo00OUflS8k//7LR0i4GBLLG88HTf5AFjTXfqYlAZzdHlFC7cQ=="
-
-      console.log("Got file name, making GET request to download image")
-
-      const resp = await fetch(url, {
-        method: 'GET',
-        header: {
-          'username': username
-        }
-      })
-      
-      console.log("Made GET request successfully")
-
-      console.log("resp:" + resp)
-
-      let data = await resp.json()
-
-      console.log("data:" + data)
-
-      let imageUrl = data.downloadUri
-
-      window.open(imageUrl, "_self")
-
-      console.log("window.open ran. imageUrl= " + imageUrl)
-
-    } catch(err) {
-      alert(err)
-    }
-
+      var data = await resp.text();
+      console.log(data);
+      $('#output').text("Your image has been stored successfully!")
   } else {
-     alert("No name error.")
+      alert("No name error.")
   }
 }
+  
+  async function downloadImage() {
+    var username = document.getElementById("downloadusername").value;
+    console.log("Attempting to get your pdf...");
+    const resp = await fetch("INSERT_DOWNLOAD)URL", {
+        method: 'GET',
+        headers: {
+            'username' : username
+        },
+    });
+  
+    var data = await resp.json();
+    console.log("PDF link received!")
+    console.log(data.downloadUri)
+    console.log(data.success)
+    const link = data.downloadUri
+    var success = data.success
+  
+    window.open(link, "_self")
+  }
